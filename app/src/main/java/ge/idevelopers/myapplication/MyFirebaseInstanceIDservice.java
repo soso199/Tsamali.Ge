551fe79f2c40e55c9185fe2eb77a566d2325f31a
soso199 <sosotukhashvili2014@ens.tsu.edu.ge>
2017-04-12 13:19:45 +0400

    tsamali.ge damatebulia hamburger button,swipe moaq informacia serveridan

---
+++ app/src/main/java/ge/idevelopers/myapplication/MyFirebaseInstanceIDservice.java
@@ -0,0 +1,22 @@
+package ge.idevelopers.myapplication;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by soso on 4/3/17.
+ */
+
+public class MyFirebaseInstanceIDservice extends FirebaseInstanceIdService {
+    private static final String TAG="MyFirebaseInsIDservice";
+
+    @Override
+    public void onTokenRefresh() {
+
+        String refreshedToken=FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG,"New Token: "+refreshedToken);
+
+    }
+}
---
+++ app/src/main/java/ge/idevelopers/myapplication/MyFirebaseMessagingService.java
@@ -0,0 +1,67 @@
+package ge.idevelopers.myapplication;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ge.idevelopers.myapplication.MainActivity;
+import ge.idevelopers.myapplication.R;
+
+/**
+ * Created by soso on 4/3/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG="MyFirebaseMessagService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        Log.d(TAG,"FROM: "+remoteMessage.getFrom());
+
+        if (remoteMessage.getData().size()>0)
+            Log.d(TAG, "Message Data: "+remoteMessage.getData());
+
+        if (remoteMessage.getNotification()!=null){
+            Log.d(TAG,"Message Body: "+remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getBody(),remoteMessage.getNotification().getTitle());
+        }
+
+
+    }
+
+
+// *** display notification ***\\\\
+    private void sendNotification(String body,String title) {
+        Intent mIntent=new Intent(this,MainActivity.class);
+        mIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent mPandingIntent=PendingIntent.getActivity(this,0,mIntent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri notificationSound= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        if(title.length()==0)
+            title="Tsamali.ge";
+
+        android.support.v4.app.NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.tsamali_logo)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(notificationSound)
+                .setContentIntent(mPandingIntent);
+
+        NotificationManager mNotificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0,mBuilder.build());
+
+    }
+}
